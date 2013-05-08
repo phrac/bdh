3926461daf1f344dfee69caf3d15359d3188abf1
Derek Musselmann <derek@disflux.com>
2013-05-08 08:21:11 -0500

    Begin writing about various gin brands

--- distillers_bible.d
+++ distillers_bible.d
@@ -1,1 +1,1 @@
-distillers_bible.dvi distillers_bible.d : distillers_bible.tex gin/overview.tex gin/ingredients.tex   disfluxpress.eps  
+distillers_bible.dvi distillers_bible.d : distillers_bible.tex gin/overview.tex gin/ingredients.tex gin/gin_brands.tex   disfluxpress.eps  
